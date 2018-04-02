64ee2a9a9fd614fb67aa6397230781904821a55c
danasselin <dasselin86@gmail.com>
2018-04-02 10:06:25 -0400

    Add ability to compare to production version of site

--- src/compareSnapshot.js
+++ src/compareSnapshot.js
@@ -15,11 +15,25 @@ const match = function (prevSnap, params) {
   return Promise.resolve()
 }
 
-module.exports = function (filename, callback) {
+const getEnvUrl = function (env, snapshot) {
+  if (env === 'production') {
+    const canonical = snapshot['Current URL'].split('pb')
+    if (canonical && canonical[1]) {
+      // eVar20 is the site name, e.g., 'latimes'
+      return `http://${snapshot['eVar20']}.com${canonical[1]}`
+    } else {
+      throw new Error('Invalid URL structure. Must contain "pb" segment to fetch production version')
+    }
+  } else {
+    throw new Error('Support for "production" environments only at this time')
+  }
+}
+
+module.exports = function (filename, callback, env) {
   const prevSnap = JSON.parse(
     fs.readFileSync(`${pathToSnapshots()}/${filename}`, 'utf8')
   )
-  const url = prevSnap['Current URL']
+  const url = env ? getEnvUrl(env, prevSnap) : prevSnap['Current URL']
   captureSnapshot(url, function (newSnap) {
     match(prevSnap, newSnap)
       .then(function () {
